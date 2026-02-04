747cbb42027fdb17b4b53addc6cdeab096132a76
Reverb <j_kroeker@reverb256.ca>
2026-02-04 16:02:23 -0600

    Remove reverb-os.cachix.org (requires signing key we don't have)

--- modules/nix-config.nix
+++ modules/nix-config.nix
@@ -42,8 +42,8 @@
         "https://devenv.cachix.org"
         # Garnix - CI/CD builds (free for open source)
         "https://cache.garnix.io"
-        # Reverb OS - Your personal Cachix cache
-        "https://reverb-os.cachix.org"
+        # Reverb OS - Your personal Cachix cache (requires signing key - disabled for now)
+        # "https://reverb-os.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -56,7 +56,7 @@
         "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "reverb-os.cachix.org-1:dctKtu02bV/4fbsYbGuVVxQo9R7X6lNqUet1qj2jYz="
+        # "reverb-os.cachix.org-1:dctKtu02bV/4fbsYbGuVVxQo9R7X6lNqUet1qj2jYz=" # Requires signing key
         # "magic.nixos.org-1:eRQ8mF8J9FqT6yV6k3kHdYiVr4R9mYr2A="
       ];
 
