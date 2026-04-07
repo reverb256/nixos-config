a7c570323d0d66b3e73d64759018c90b5767a368
Jeremy Kroeker <j_kro@local>
2026-04-07 02:08:01 -0500

    fix: deduplicate k3s nvidia.enable on all GPU nodes

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -225,10 +225,7 @@
       serverAddr = "https://10.1.1.100:6443";
       tokenFile = "/run/agenix/k3s-cluster-token";
       nodeIP = "10.1.1.110";
-      nvidia.enable = true;
-      nvidia.enable = true;
       calico.enable = true;
-      nvidia.enable = true;
     };
 
     # Keepalived VIP for HA API server access
