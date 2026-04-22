0200be3991ef84f1828989e7255cfde59162389c
Jeremy Kroeker <j_kro@local>
2026-04-22 02:38:26 -0500

    tailscale: add SSH flag, seeker.lan DNS, firewall fix

--- hosts/zephyr/firewall.nix
+++ hosts/zephyr/firewall.nix
@@ -28,7 +28,7 @@
       ];
       # Workaround: list merge is broken for this host. Ensure critical ports via nft rules.
       extraInputRules = ''
-        tcp dport { 1235, 1236, 1237, 8080, 8888, 3900, 3901, 50000, 9100, 9400 } accept
+        tcp dport { 1235, 1236, 1237, 53317, 8080, 8888, 3900, 3901, 50000, 9100, 9400 } accept
       '';
       allowedUDPPorts = lib.mkOptionDefault [
         9757
