d0cae014f06bcc297e4761a824af764f2cc4fb7c
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-05-18 04:11:19 +0000

    auto-update: LM Studio 0.4.13-1

--- packages/lmstudio.nix
+++ packages/lmstudio.nix
@@ -4,10 +4,10 @@
   lib,
   runCommandLocal,
 }: let
-  version = "0.4.12-1";
+  version = "0.4.13-1";
   src = fetchurl {
     url = "https://installers.lmstudio.ai/linux/x64/${version}/LM-Studio-${version}-x64.AppImage";
-    sha256 = "sha256-U7TJkMUqmL4Wk77zcIN2/4IFz7artvVg0saREjoGy8I=";
+    sha256 = "sha256-IHhqAsYVi1XCaryxrEyhakDyye2vehbsJ77eF68KaIg=";
   };
   appimageContents = appimageTools.extractType2 {
     pname = "lmstudio";
