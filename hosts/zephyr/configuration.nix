0292b2a1927394725365d965d48350732c1a25ad
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-05-25 05:11:09 -0500

    fix(zephyr): remove stale nixos-share server config from configuration.nix

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -169,11 +169,7 @@ in {
   };
 
    services.nixos-share = {
-     enable = true;
-     server.enable = true;
-     server.exports = [
-       { path = "/etc/nixos"; description = "NixOS configuration"; }
-     ];
+     enable = false;
    };
 
   i18n.defaultLocale = "en_CA.UTF-8";
