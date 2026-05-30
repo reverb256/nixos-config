ca9502432ad539ac0e94fdcc3e21cf7c203c8c9a
Jeremy Kroeker <jkroeker@proton.me>
2026-05-29 22:06:48 -0500

    Fix syntax error in kubernetes import (use regular argument set)

--- flake.nix
+++ flake.nix
@@ -371,7 +371,7 @@
 packages.x86_64-linux.maplespike-engine-image = pkgsWithOverlay.maplespike-engine-image;
      # hermes-workspace-image and hermes-webui-image archived (2026-05-16)
      overlays.default = (import ./overlay.nix) {inherit inputs;};
-     kubernetes = import ./kubernetes {inherit pkgs pkgsWithOverlay inputs lib = inputs.nixpkgs.lib;};
+     kubernetes = import ./kubernetes {lib = inputs.nixpkgs.lib, inherit pkgs pkgsWithOverlay inputs;};
 
     apps.x86_64-linux.k8s-validate = {
       type = "app";
