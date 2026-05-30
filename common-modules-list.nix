10d235216d81ae4f69c80a02f0223fa8a3ebe736
Jeremy Kroeker <jkroeker@proton.me>
2026-05-30 13:02:06 -0500

    Fix ai-gateway input URL (use github instead of path)

--- common-modules-list.nix
+++ common-modules-list.nix
@@ -13,6 +13,8 @@
   inputs.caddy-ingress.nixosModules.caddy
   inputs.caddy-ingress.nixosModules.caddy-common
 
+  inputs.ai-gateway.nixosModules.default
+
   inputs.compute-market.nixosModules.default
 
   inputs.gpu-proxy.nixosModules.default
