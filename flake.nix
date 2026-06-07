ff2693a0667639ac1057f9cc147b958ad4e83c3b
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-06-07 14:35:05 -0500

    Pin home-manager to release-26.05 commit e28654b7 (2026-06-02)

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/9b696460ac78b5ccfc17c854d8c976f20456e943";
     home-manager = {
-      url = "tarball+https://codeload.github.com/nix-community/home-manager/tar.gz/509ed3c603349a9d43de9e2ae6613baea6bd5b34";
+      url = "tarball+https://codeload.github.com/nix-community/home-manager/tar.gz/e28654b71096e08c019d4861ca26acb646f583d8";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
