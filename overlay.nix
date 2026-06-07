51073db2f65358de01552a0d1be935577d946f78
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-06-07 14:25:47 -0500

    Fix gradio test override (use overridePythonAttrs)

--- overlay.nix
+++ overlay.nix
@@ -118,7 +118,7 @@ _final: prev:
       };
       # Fix: pipx 1.8.0 test failures (spaces around @)
       pipx = py-super.pipx.overridePythonAttrs { doCheck = false; };
-      gradio = py-super.gradio.overrideAttrs (_old: { doCheck = false; checkPhase = false; });
+      gradio = py-super.gradio.overridePythonAttrs { doCheck = false; };
     };
   };
 
@@ -141,11 +141,3 @@ _final: prev:
   privacy-filter = prev.callPackage ./packages/privacy-filter.nix {
     transformers-dev = prev.callPackage ./packages/transformers-dev.nix { };
   };
-  srbminer-multi = prev.callPackage ./packages/srbminer.nix {};
-  # dufs tests require CA certificates for reqwest-based HTTP client
-  # Disabled tests - they fail with network requirements
-  dufs = prev.dufs.overrideAttrs (old: {
-    doCheck = false;
-    checkPhase = "";
-  });
-}
