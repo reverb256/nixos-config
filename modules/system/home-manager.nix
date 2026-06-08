edc25e08f1df5d462a93ea19d62fc7f5a49b1d88
Jeremy Kroeker <jkroeker@proton.me>
2026-06-08 05:47:22 -0500

    fix: starship config errors + hermes skin from stylix
    Starship fixes:
    - Time format: use Nix '' strings for correct \[\] escaping
    - Directory: fish_style_pwd_dir_length instead of fish_style_pwd_rooted
    - Custom miner: remove invalid require_commands key (non-zero exit hides module)
    
    Hermes skin:
    - New module generates ~/.hermes/skins/stylix.yaml from stylix colors
    - Maps all skin colors (banner, ui, status bar, completion menu, etc.)
    - Auto-adapts to each host's theme

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -37,6 +37,7 @@ in {
         ../../modules/home-manager/opencode.nix
         ../../modules/home-manager/firefox-pwa-apps.nix
         ../../modules/home-manager/alacritty.nix
+        ../../modules/home-manager/hermes-skin.nix
         ../../modules/home-manager/icon-theme.nix
         ../../modules/home-manager/dolphin.nix
         ../../modules/home-manager/desktop-utilities.nix
