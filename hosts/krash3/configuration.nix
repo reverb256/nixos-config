f2c7b24afa1842f8b44f42629d78644ff2c59f1b
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-06-25 18:10:21 -0500

    fix: pin VM memory with memoryBacking locked, add cache writeback, startupPolicy optional

--- hosts/krash3/configuration.nix
+++ hosts/krash3/configuration.nix
@@ -8,6 +8,9 @@ let
       <memory unit='KiB'>25165824</memory>
       <currentMemory unit='KiB'>25165824</currentMemory>
       <vcpu placement="static">18</vcpu>      <cputune>        <vcpupin vcpu="0" cpuset="0"/>        <vcpupin vcpu="1" cpuset="1"/>        <vcpupin vcpu="2" cpuset="2"/>        <vcpupin vcpu="3" cpuset="3"/>        <vcpupin vcpu="4" cpuset="4"/>        <vcpupin vcpu="5" cpuset="5"/>        <vcpupin vcpu="6" cpuset="6"/>        <vcpupin vcpu="7" cpuset="7"/>        <vcpupin vcpu="8" cpuset="8"/>        <vcpupin vcpu="9" cpuset="12"/>        <vcpupin vcpu="10" cpuset="13"/>        <vcpupin vcpu="11" cpuset="14"/>        <vcpupin vcpu="12" cpuset="15"/>        <vcpupin vcpu="13" cpuset="16"/>        <vcpupin vcpu="14" cpuset="17"/>        <vcpupin vcpu="15" cpuset="18"/>        <vcpupin vcpu="16" cpuset="19"/>        <vcpupin vcpu="17" cpuset="20"/>      </cputune>
+      <memoryBacking>
+        <locked/>
+      </memoryBacking>
       <os>
         <type arch='x86_64' machine='pc-q35-10.2'>hvm</type>
         <loader readonly='yes' type='pflash' format='raw'>/run/libvirt/nix-ovmf/edk2-x86_64-code.fd</loader>
