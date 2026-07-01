a18973eeb797f7996eb321ba96128e0c546c2802
Jeremy Kroeker <j_kroeker@reverb256.ca>
2026-07-01 10:20:48 -0500

    fix(k3s): krash3 token from /run/secrets (no /persistent mount)

--- hosts/krash3/services.nix
+++ hosts/krash3/services.nix
@@ -9,7 +9,7 @@ in {
     role = "agent";
     nodeName = "krash3";
     nodeIP = "10.1.1.150";
-    tokenFile = "/persistent/etc/k3s-cluster-token";
+    tokenFile = "/run/secrets/k3s-cluster-token";
     flannelIface = "enp7s0";
   };
 
