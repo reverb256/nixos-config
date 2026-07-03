2ea55d124f38cf77f1c2ccee5743a78004dcba9d
j_kro <j_kro@localhost>
2026-07-03 03:47:47 -0500

    peakminer: combine wallet and worker into --user flag (Kryptex dot-separated format)

--- modules/services/peakminer.nix
+++ modules/services/peakminer.nix
@@ -155,8 +155,7 @@ in {
                 exec ${pkgs.peakminer}/bin/peakminer \
                   ${lib.concatStringsSep " " cfg.extraArgs} \
                   --url ${poolUrl} \
-                  --user ${instanceWallet} \
-                  --worker ${instance.name} \
+                  --user ${instanceWallet}.${instance.name} \
                   --devices ${instance.devices} \
                   --api-port ${toString instance.apiPort} \
                   --gpu-temp-stop ${toString instance.tempStop} \
