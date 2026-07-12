b42f3b1fbdd2759c95f986dc7866e55ce9108002
Hermes <[email protected]>
2026-07-12 01:43:47 -0500

    haven: pin mosiac to v0.1.6-forum (known-good build; :latest manifest corrupted in registry)

--- kubernetes/modules/haven.nix
+++ kubernetes/modules/haven.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: let
-  havenImage = "nexus:5000/mosiac:latest";
+  havenImage = "nexus:5000/mosiac:v0.1.6-forum";
   managed = {
     "app.kubernetes.io/managed-by" = "easykubenix";
   };
