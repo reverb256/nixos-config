56ea0ae4454e89e01c83fd6aac6f9ade6804652a
Hermes <[email protected]>
2026-07-14 04:52:33 -0500

    feat: replace broken untitled scheme with proper base16 osaka-jade theme; fix starship palette

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -94,24 +94,7 @@ in {
   boot.binfmt.emulatedSystems = ["i686-linux"];
 
   stylix = {
-    base16Scheme = {
-      base00 = "111c18";
-      base01 = "23372B";
-      base02 = "53685B";
-      base03 = "53685B";
-      base04 = "ACD4CF";
-      base05 = "C1C497";
-      base06 = "D7C995";
-      base07 = "F6F5DD";
-      base08 = "FF5345";
-      base09 = "db9f9c";
-      base0A = "E5C736";
-      base0B = "549e6a";
-      base0C = "2DD5B7";
-      base0D = "509475";
-      base0E = "D2689C";
-      base0F = "9eebb3";
-    };
+    base16Scheme = /etc/nixos/modules/desktop/themes/osaka-jade.yaml;
   };
 
   networking = {
