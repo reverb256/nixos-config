93ca4f66a8efbfa2f023c34299bcb20c49338e37
Hermes <[email protected]>
2026-07-21 11:51:07 -0500

    fix: restructure mining module to avoid Lix };-as-terminator parser bug

--- modules/mining/mining.nix
+++ modules/mining/mining.nix
@@ -8,30 +8,30 @@
 with lib;
 let
   cfg = config.services.mining;
-in
-{
-  options.services.mining = {
-    enable = mkEnableOption "Mining services (peakminer GPU mining)";
-    user = mkOption {
-      type = types.str;
-      default = "mining";
-      description = "User to run mining services as";
+  body = {
+    options.services.mining = {
+      enable = mkEnableOption "Mining services (peakminer GPU mining)";
+      user = mkOption {
+        type = types.str;
+        default = "mining";
+        description = "User to run mining services as";
+      };
+    };
+
+    config = mkIf cfg.enable {
+      users.users.${cfg.user} = {
+        isSystemUser = true;
+        group = "mining";
+        extraGroups = ["video" "render"];
+      };
+      users.groups.mining = { };
+
+      boot.kernel.sysctl."vm.nr_hugepages" = 1280;
+
+      systemd.tmpfiles.rules = [
+        "d /var/lib/mining 0750 ${cfg.user} mining -"
+        "d /var/log/mining 0750 ${cfg.user} mining -"
+      ];
     };
   };
-
-  config = mkIf cfg.enable {
-    users.users.${cfg.user} = {
-      isSystemUser = true;
-      group = "mining";
-      extraGroups = ["video" "render"];
-    };
-    users.groups.mining = { };
-
-    boot.kernel.sysctl."vm.nr_hugepages" = 1280;
-
-    systemd.tmpfiles.rules = [
-      "d /var/lib/mining 0750 ${cfg.user} mining -"
-      "d /var/log/mining 0750 ${cfg.user} mining -"
-    ];
-  };
-}
+in body
