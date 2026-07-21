9734aa60dd0b65c188390a9422469b827303181c
Hermes <[email protected]>
2026-07-21 03:05:53 -0500

    fix(flake): convert aagl-gtk-on-nix input to git+https to bypass GitHub API 401

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     aagl = {
-      url = "github:ezKEa/aagl-gtk-on-nix";
+      url = "git+https://github.com/ezKEa/aagl-gtk-on-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
