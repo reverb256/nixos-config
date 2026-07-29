2a5960af1f0dc82dcd66b457d5736c6a1fe312eb
Hermes <[email protected]>
2026-07-29 18:33:36 -0500

    feat(gitlawb): add option-4 flake (packages + overlay + NixOS module)
    - pkgs/gitlawb/flake.nix: builds gl, git-remote-gitlawb, gitlawb-node from source via rust-bin + buildRustPackage
    - pkgs/gitlawb/nixos-module.nix: programs.gitlawb module wiring gl + remote helper into git config
    - flake.nix: add gitlawb = path:./pkgs/gitlawb input
    - hosts/zephyr/configuration.nix: import inputs.gitlawb.nixosModule (replaces old container-only gitlawb-node.nix)
    
    Validated standalone: nix flake show resolves packages + nixosModule on all default systems.

---
+++ pkgs/gitlawb/flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "gitlawb Nix flake: packages, overlay, NixOS module";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, systems, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib stdenv fetchurl rust-bin;
+        rust-toolchain = rust-bin.stable.latest.default.override { targets = [ system ]; };
+        platform = pkgs.makeRustPlatform {
+          inherit rust-toolchain;
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        };
+        gitlawb-src = fetchurl {
+          url = "https://github.com/gitlawb/node/archive/refs/tags/v0.7.0.tar.gz";
+          sha256 = "sha256-3F5qmHxn4A/3Y4GWjBBP7v7yWVfJR5MQT59qZ3Ix/qI=";
+        };
+        buildGitlawbCrate = crateName: platform.buildRustPackage rec {
+          pname = crateName;
+          version = "0.7.0";
+          buildInputs = [ pkgs.git pkgs.openssl ];
+          src = gitlawb-src;
+          sourceRoot = "node-0.7.0";
+          cargoBuildFlags = [ "-p" crateName ];
+          cargoTestFlags = [ "-p" crateName ];
+          doCheck = false;
+          meta = with lib; {
+            description = "Gitlawb crate: ${crateName}";
+            homepage = "https://gitlawb.com";
+            license = licenses.mit;
+            platforms = platforms.linux;
+            maintainers = [ ];
+          };
+        };
+        gl = buildGitlawbCrate "gl";
+        git-remote-gitlawb = buildGitlawbCrate "git-remote-gitlawb";
+        gitlawb-node = buildGitlawbCrate "gitlawb-node";
+      in {
+        packages = {
+          inherit gl git-remote-gitlawb gitlawb-node;
+          default = gl;
+        };
+
+        nixosModule = ./nixos-module.nix;
+
+        overlays.default = final: prev: {
+          gitlawb = {
+            inherit gl git-remote-gitlawb gitlawb-node;
+            package = gl;
+            remoteHelper = git-remote-gitlawb;
+            node = gitlawb-node;
+          };
+        };
+      });
+}
