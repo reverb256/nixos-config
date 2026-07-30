ee66c298aa912e07e11fbe97f081d4c197dabefd
Hermes <[email protected]>
2026-07-30 15:14:20 -0500

    fix(cluster): remove NixOS-only steam/discord/obs from standalone HM zephyr

--- modules/home-manager/standalone-zephyr.nix
+++ modules/home-manager/standalone-zephyr.nix
@@ -11,9 +11,6 @@
     vkbasalt
   ];
 
-  programs.steam.enable = true;
-  programs.discord.enable = true;
-  programs.obs-studio.enable = true;
 
   home.file.".local/share/Steam/steamapps/common".source = pkgs.linkFarm "steam-games" {};
 }
