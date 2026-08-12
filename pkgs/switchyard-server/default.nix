55b9e21633aa7cfdfa05e1575041afea9f82b04e
Hermes <[email protected]>
2026-08-11 20:18:09 -0500

    fix(switchyard): use lib.fakeSha256 placeholder for hash capture

--- pkgs/switchyard-server/default.nix
+++ pkgs/switchyard-server/default.nix
@@ -6,7 +6,7 @@ rustPlatform.buildRustPackage rec {
     pname = "switchyard-server";
     version = version;
     # sha256 and cargoHash will be filled in from the first nix-build run error message.
-    sha256 = "sha256-placeholder";
+    sha256 = lib.fakeSha256;
   };
   cargoHash = "cargoHash-placeholder";
   meta = with lib; {
