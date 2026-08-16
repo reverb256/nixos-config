1ed8abba02052ed71057df2627c694388d780e50
Hermes <[email protected]>
2026-08-16 00:31:51 -0500

    fix: add nftables crashfix overlay from upstream master
    Patches nftables 1.1.6 with upstream master crash fixes:
    - parser_json: fix map/set type confusion crash
    - json: work around fuzzer-induced assert crashes
    - netlink: Avoid crash upon missing attributes
    - mnl: catch bogus expressions before crashing
    
    NixOS/nixpkgs#500465, kubernetes/kubernetes#136786

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,6 +10,7 @@ let
   pythonOverlay = import ./python.nix { inherit inputs _final prev; };
   hardwareOverlay = import ./hardware.nix { inherit inputs _final prev; };
   llamaOverlay = import ./llama.nix { inherit inputs _final prev; };
+  nftablesOverlay = import ./nftables-crashfix.nix { inherit inputs _final prev; };
 in
 foldl' (acc: overlay: acc // overlay) {} [
   bugfixOverlay
@@ -17,4 +18,5 @@ foldl' (acc: overlay: acc // overlay) {} [
   pythonOverlay
   hardwareOverlay
   llamaOverlay
+  nftablesOverlay
 ]
