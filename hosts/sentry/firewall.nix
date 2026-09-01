fc696998b95cdb8c7d5aad98be0f60da27c84e27
Reverb <j_kroeker@reverb256.ca>
2026-09-01 10:24:44 -0500

    security: restrict sentry firewall to tailnet+pod CIDR (LAN removed)
    Per security audit 2026-09-01. 9100/3900/3901/8001-8003/22000/11434 now
    source-restricted to 100.64.0.0/10 (tailnet) + 10.42.0.0/16 (pod) instead
    of 10.1.1.0/24 (LAN). Requires nixos-rebuild on sentry to take effect.
    Live nft drop rule applied as interim.

--- hosts/sentry/firewall.nix
+++ hosts/sentry/firewall.nix
@@ -2,10 +2,10 @@
   networking = {
     firewall = {
       extraInputRules = lib.mkAfter ''
-        # Source-restrict exposed ports to LAN + pod CIDR
-        ip saddr { 10.1.1.0/24, 10.42.0.0/16 } tcp dport { 1235, 4180, 3100, 3900, 3901, 9100, 8001, 8002, 8003 } accept
-        ip saddr { 10.1.1.0/24, 10.42.0.0/16 } tcp dport 22000 accept
-        ip saddr { 10.1.1.0/24, 10.42.0.0/16 } tcp dport 11434 accept
+        # Source-restrict exposed ports to tailnet + pod CIDR (LAN removed 2026-09-01, security audit)
+        ip saddr { 100.64.0.0/10, 10.42.0.0/16 } tcp dport { 1235, 4180, 3100, 3900, 3901, 9100, 8001, 8002, 8003 } accept
+        ip saddr { 100.64.0.0/10, 10.42.0.0/16 } tcp dport 22000 accept
+        ip saddr { 100.64.0.0/10, 10.42.0.0/16 } tcp dport 11434 accept
       '';
       allowedTCPPortRanges = lib.mkOptionDefault [
         {
